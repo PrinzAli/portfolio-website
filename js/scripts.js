6dfc6502839fff83f04957db6e7a19c95f08093b
PrinzAli <73790573+PrinzAli@users.noreply.github.com>
2021-01-03 16:21:01 +0100

    added mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 console.log("Hi! Welcome to My Portfolio Site - Have a look around!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+ if (x.className === 'navtoggle') {x.className += 'responsive';
+} else {x.className = 'navtoggle';}} 
