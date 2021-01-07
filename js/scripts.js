c3ec8201914c5b93ef33e5a0175aa937cde16596
PrinzAli <73790573+PrinzAli@users.noreply.github.com>
2021-01-07 19:49:39 +0100

    error fixing

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hi! Welcome to My Portfolio Site - Have a look around!")
+console.log('Hi! Welcome to My Portfolio Site - Have a look around!')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
